f1fc04328023f263332c6634cedc957f2b702532
Tracywang <mirror_heat@163.com>
2015-12-19 11:26:48 +0800

    modify some problems

--- hash_method/estimate.py
+++ hash_method/estimate.py
@@ -6,37 +6,52 @@
 
 # precise = e/r
 
-import psycopg2
+# import psycopg2
 import rs
 import tagUtil
+import csv
+import datetime
 
 class estimate:
 
   # def __init__(self):
 
 
-  def getCursor(self):
-    conn_str = "host='localhost' dbname='tagrecsys' user='dbuser' password='dbuser'"
-    print "Connecting to database\n ->%s" % (conn_str)
-    conn = psycopg2.connect(conn_str)
-    cursor = conn.cursor()
-    print "Connected!\n"
-    return cursor
+  # def getCursor(self):
+  #   conn_str = "host='localhost' dbname='tagrecsys' user='dbuser' password='dbuser'"
+  #   print "Connecting to database\n ->%s" % (conn_str)
+  #   conn = psycopg2.connect(conn_str)
+  #   cursor = conn.cursor()
+  #   print "Connected!\n"
+  #   return cursor
 
   def recordUserRecommends(self):
     user = tagUtil.tagUtil()
     userlist = user.userlist()
     res = rs.rs()
     recordlist = []
+    print 'estimate start,datetime:'+ str(datetime.datetime.now())
+
     for u in userlist:
       r = len(user.userMovies(u))
       e = res.getRecommendMovies(u)[1]
       recordlist.append([u,float(e)/r,r,e])
-    with open('./estimate.csv','w',newline='') as f:
+
+    print 'estimate end,datetime:'+ str(datetime.datetime.now())
+
+    self.writeFile(recordlist)
+
+  def writeFile(self,recordlist):
+    # recordlist=[[1,2,3],[2,2,3]]
+    with open('./estimate.csv','w') as f:
       a = csv.writer(f,delimiter=',')
-      a.wirterows(recordlist)
+      a.writerows(recordlist)
 
 def main():
   es = estimate()
   es.recordUserRecommends()
+
+
+
+
 
